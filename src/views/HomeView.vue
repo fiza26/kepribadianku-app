79a41a717af84f27b21adbf15c16345a4b9fbe43
Fiza Ahmad Baizuri <fizaahmadbaizuri@gmail.com>
2024-11-08 13:24:18 +0700

    added new question in json files and updated the logic for both correct and incorrect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -96,7 +96,8 @@ const scoreChanges = {
     { creative: 10 },               // Question 7
     { creative: 10, logician: 10 }, // Question 8
     { logician: 10 },               // Question 9
-    { logician: 10, feeler: 5, creative: 5 }                // Question 10
+    { logician: 10, feeler: 5, creative: 5 },                // Question 10
+    { feeler: 10 }                  // Question 11
   ],
   incorrect: [
     { feeler: 10 },                 // Question 0
@@ -108,8 +109,9 @@ const scoreChanges = {
     {},                             // Question 6 (no score changes)
     { logician: 5, feeler: 5 },     // Question 7
     { logician: 5, feeler: 10 },    // Question 8
-    { feeler: 5 },                   // Question 9
-    { creative: 10, logician: 5, feeler: 5 }              // Question 10
+    { feeler: 5 },                  // Question 9
+    { creative: 10, logician: 5, feeler: 5 },              // Question 10
+    { logician: 10, creative: 5 }                              // Question 11
   ]
 };
 
